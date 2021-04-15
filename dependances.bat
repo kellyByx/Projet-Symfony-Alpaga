d8bc257fa1f016e3905af0e551e92876c899622d
Kelly Byx <kelzoe@skynet.be>
2021-04-15 15:02:01 +0200

    base projet, ajout fichiers bat

---
+++ dependances.bat
@@ -0,0 +1,6 @@
+​composer require annotations
+composer require symfony/orm-pack
+composer require --dev symfony/maker-bundle
+composer require symfony/apache-pack
+composer require symfony/maker-bundle --dev
+composer require --dev doctrine/doctrine-fixtures-bundle
